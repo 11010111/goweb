18b204374d6a5edd6c0f88ea5c117124a757722d
Konstantin Schneider <schneider.konstantin@outlook.com>
2024-01-07 19:10:09 +0100

    add tests

---
+++ types/text_test.go
@@ -0,0 +1,22 @@
+package types_test
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/11010111/goweb/types"
+)
+
+func TestText(t *testing.T) {
+	text := types.NewText()
+	text.SetHeadline("Text Headline")
+	text.SetBodytext("Text Bodytext")
+
+	if strings.Compare(text.GetHeadline(), "Text Headline") != 0 {
+		t.Error("Headline is wrong")
+	}
+
+	if strings.Compare(text.GetBodytext(), "Text Bodytext") != 0 {
+		t.Error("Bodytext is wrong")
+	}
+}
